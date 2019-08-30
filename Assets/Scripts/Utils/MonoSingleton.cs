9ef99d3c01867520339bfc9e552f385dc707955a
Michal <michal_szkopinski@op.pl>
2019-08-30 19:23:19 +0200

    Created base MonoSingleton, added DoTween plugin

---
+++ Assets/Scripts/Utils/MonoSingleton.cs
@@ -0,0 +1,33 @@
+using System.Linq;
+using UnityEngine;
+	
+public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	[SerializeField]
+	bool persistBetweenScenes = true;
+	
+	public static T Instance
+	{
+		get
+		{
+			if(instance != null) return instance;
+
+			var components = Object.FindObjectsOfType<T>().ToList();
+			instance = components.Count > 0 
+				? components.FirstOrDefault() 
+				: new GameObject($"S{typeof(T)}_SINGLETON").AddComponent<T>();
+			
+			return instance;
+		}
+	}
+
+	static T instance;
+
+	protected virtual void Awake()
+	{
+		if(persistBetweenScenes)
+		{
+			DontDestroyOnLoad(this);
+		}
+	}
+}
