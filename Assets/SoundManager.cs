c9d9615f00b23256e44318fda7e3e70ef1bceb94
tomaszm.gorny <tomaszm.gorny@gmail.com>
2019-09-07 20:33:35 +0200

    sounds etc

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WSGJ.Utils;
+
+public class SoundManager : MonoSingleton<SoundManager>
+{
+    // Start is called before the first frame update
+
+    [SerializeField] private AudioClip Theme;
+    [SerializeField] private AudioClip CatapultShot;
+    [SerializeField] private AudioClip BlockHitsTheCart;
+    [SerializeField] private AudioClip BoxingHit;
+    [SerializeField] private AudioClip BlockHitsOtherBlock;
+    [SerializeField] private AudioClip EnemyDies;
+
+    
+    void Start()
+    {
+        AudioSource audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Theme;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
